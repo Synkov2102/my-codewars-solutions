f3faa6ec6137af77fb72f3eb1c0f60b5a52f4ee7
Eugeny <synkoveugeny@gmail.com>
2022-03-07 13:37:39 +0300

    Решение kata 6kyu про Прыгающий мяч

--- 5kyu/Simple Pig Latin.js
+++ 5kyu/Simple Pig Latin.js
@@ -1,17 +1,17 @@
 //Переместите первую букву каждого слова в конец, 
 //а затем добавьте «ay» в конец слова. Оставьте знаки препинания нетронутыми.
 
-function pigIt(str){
+function pigIt(str) {
     let arr = str.split(' ').map(word => word.split(''));
 
-   return arr.map((word)=>{
-       let newWord = []
-        if (!((97 <= word[word.length-1].charCodeAt(0) && word[word.length-1].charCodeAt(0)  <= 122)||(65 <= word[word.length-1].charCodeAt(0) && word[word.length-1].charCodeAt(0) <= 90))){
+    return arr.map((word) => {
+        let newWord = []
+        if (!((97 <= word[word.length - 1].charCodeAt(0) && word[word.length - 1].charCodeAt(0) <= 122) || (65 <= word[word.length - 1].charCodeAt(0) && word[word.length - 1].charCodeAt(0) <= 90))) {
             if (word.length == 1) {
                 return word.join('')
             }
-            Array.prototype.push.apply(newWord, word.slice(1, word.length-1))
-            newWord.push(word[0], 'a', 'y', word[word.length-1])
+            Array.prototype.push.apply(newWord, word.slice(1, word.length - 1))
+            newWord.push(word[0], 'a', 'y', word[word.length - 1])
             return newWord.join('')
         }
         else {
